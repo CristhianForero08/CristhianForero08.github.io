231e55f44310fa96a3f3f290cb10162225392240
Your Name <cristhianforerobello@gmail.com>
2024-05-03 19:49:04 -0500

    Actualizacion version 0.06

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -14,7 +14,14 @@ const Contacto = () => {
                   <Boton texto='Whatspp' link='https://wa.me/573004548869?text=Hola,%20Cristhian'/>
                   <Boton texto='cristhian@cristhianforero.com' link='mailto:cristhianforerobello@gmail.com?subject=Asunto del correo'/>
                   
-
+                  <ul className="redes-principal">
+                      <a href="http://www.linkedin.com/in/cristhianforero" target='_blank'><i className="fa-brands fa-linkedin lis"></i></a>
+                      <a href="https://github.com/CristhianForero08" target='_blank'><i className="fa-brands fa-github lis"></i></a>
+                      <a href="https://www.instagram.com/ing_cristhian_forero/" target='_blank'><i className="fa-brands fa-square-instagram lis"></i></a>
+                      <a href="https://neogyck.com/" target='_blank'><i className="fa-solid fa-globe lis"></i></a>
+                      
+                      
+                    </ul>
                   
                 </div>
 
